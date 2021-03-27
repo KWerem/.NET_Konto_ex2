ee197a1f35316472e469fe37ac742f088ab0be02
KWerem <74089244+KWerem@users.noreply.github.com>
2021-03-27 19:46:37 +0100

    Add files via upload

---
+++ Konto/Konto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Konto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account k = new Account("Jan Kowalski", "37562", "mała", "czarna");
+            k.Zaloguj("mała", "czarna");
+            Console.WriteLine(k);
+
+            k.Wpłać(500);
+            k.Wypłać(316);
+
+            k.ZmieńHasło("latte");
+            k.Zaloguj("mała", "latte");
+            Console.WriteLine(k);
+
+            Console.WriteLine("\nNaciśnij <Enter>, aby wylogowac");
+            while (Console.ReadKey().Key == ConsoleKey.Enter) { k.Wyloguj(); }
+
+
+            //Account o = new Operacje(500)
+
+
+        }
+    }
+}
